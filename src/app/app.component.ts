250251a18e176ffc7520e7133a926b1da55ccf02
廖健傑 <a36118@adcmuh.org.tw>
2021-07-15 14:28:16 +0800

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
+
+  data = [
+    ['1', 'a', 'aa'],
+    ['2', 'b', 'bb'],
+    ['3', 'c', 'cc']
+  ];
+
+  inputdata = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  // 匯出
+  daochu(){
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+    const ws2: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.utils.book_append_sheet(wb, ws2, 'Sheet2');
+
+    console.log(wb)
+    /* save to file */
+    XLSX.writeFile(wb, 'SheetJS.xlsx');
+  }
+
+  // 匯入
+  public daoru(evt: any): void {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.inputdata = (XLSX.utils.sheet_to_json(ws, {header: 1}));
+      console.log(this.inputdata);
+
+      evt.target.value = ""; // 清空
+    };
+    reader.readAsBinaryString(target.files[0]);
+
+    console.log(reader.result);
+  }
 }
